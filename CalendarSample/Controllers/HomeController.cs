53eafe6dc23ebc7672b65eabe4e36a29e6f8f5d3
uhilari <uhilari@gmail.com>
2017-10-19 22:14:07 -0500

    Iniciando el codigo

---
+++ CalendarSample/Controllers/HomeController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CalendarSample.Controllers
+{
+  public class HomeController : Controller
+  {
+    // GET: Home
+    public ActionResult Index()
+    {
+      return View();
+    }
+
+    public ActionResult Eventos(string s, string e)
+    {
+      DateTime start = DateTime.Parse(s);
+      DateTime end = DateTime.Parse(e);
+      return Json(new object[]
+      {
+        new { title = "Tarea", start = start.AddDays(5), end = start.AddDays(7) },
+        new { title = "Evento", start = start.AddDays(6), end = start.AddDays(9) },
+        new { title = "Reunion", start = start.AddDays(10), end = start.AddDays(15) }
+      }, JsonRequestBehavior.AllowGet);
+    }
+  }
+}
